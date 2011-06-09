4a2d43a7d8ec38e0977a117d2eac89d862bc2e69
Emmanuel Blot <eblot@neotion.com>
2011-06-09 14:56:30 +0200

    Remove system SQLite linkage

--- Library/Formula/subversion.rb
+++ Library/Formula/subversion.rb
@@ -97,7 +97,6 @@ class Subversion < Formula
             "--prefix=#{prefix}",
             "--with-ssl",
             "--with-zlib=/usr",
-            "--with-sqlite=/usr",
             # use our neon, not OS X's
             "--disable-neon-version-check",
             "--disable-mod-activation",
