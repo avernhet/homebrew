43c076160753294adf4f517f1f3aab508ca948c0
Emmanuel Blot <emmanuel.blot@free.fr>
2011-07-04 12:51:25 +0200

    Fix build under Lion where getline() is already defined as a system function in stdio.h

--- Library/Formula/ldapvi.rb
+++ Library/Formula/ldapvi.rb
@@ -10,8 +10,50 @@ class Ldapvi < Formula
   depends_on 'popt'
   depends_on 'readline'
 
+  def patches
+    DATA
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
     system "make install"
   end
 end
+__END__
+diff -ur a/common.h b/common.h
+--- a/common.h	2007-05-05 12:17:26.000000000 +0200
++++ b/common.h	2011-07-04 12:47:59.000000000 +0200
+@@ -273,7 +273,7 @@
+ char *home_filename(char *name);
+ void read_ldapvi_history(void);
+ void write_ldapvi_history(void);
+-char *getline(char *prompt, char *value);
++char *getline1(char *prompt, char *value);
+ char *get_password();
+ char *append(char *a, char *b);
+ void *xalloc(size_t size);
+diff -ur a/ldapvi.c b/ldapvi.c
+--- a/ldapvi.c	2007-05-05 12:17:26.000000000 +0200
++++ b/ldapvi.c	2011-07-04 12:47:59.000000000 +0200
+@@ -470,7 +470,7 @@
+ 		bo->authmethod = LDAP_AUTH_SASL;
+ 		puts("Switching to SASL authentication.");
+ 	}
+-	bo->sasl_mech = getline("SASL mechanism", bo->sasl_mech);
++	bo->sasl_mech = getline1("SASL mechanism", bo->sasl_mech);
+ }
+ 
+ static int
+diff -ur a/misc.c b/misc.c
+--- a/ldapvi-1.7/misc.c	2007-05-05 12:17:26.000000000 +0200
++++ b/misc.c	2011-07-04 12:47:59.000000000 +0200
+@@ -315,7 +315,7 @@
+ }
+ 
+ char *
+-getline(char *prompt, char *value)
++getline1(char *prompt, char *value)
+ {
+ 	tdialog d;
+ 	init_dialog(&d, DIALOG_DEFAULT, prompt, value);
+
