6f5c0c98bf31d7417580236f7e3956a84da04d25
Emmanuel Blot <emmanuel.blot@free.fr>
2011-09-17 10:00:19 +0200

    Sync GDB package name with other ARM-EABI packages, Update to GDB 7.3.1 Link with libmpc Use the regular LLVM compiler has it does not crash anymore (on Lion at least)

--- Library/Formula/gdb-arm-eabi.rb
+++ Library/Formula/gdb-arm-eabi.rb
@@ -1,24 +1,24 @@
 require 'formula'
 
-class GdbArmEcos <Formula
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.2.tar.bz2'
+class GdbArmEabi <Formula
+  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.3.1.tar.bz2'
   homepage 'http://www.gnu.org/software/gdb/'
-  md5 '64260e6c56979ee750a01055f16091a5'
+  md5 'b89a5fac359c618dda97b88645ceab47'
 
   depends_on 'gmp'
   depends_on 'mpfr'
   depends_on 'ppl'
+  depends_on 'libmpc'
   depends_on 'cloog-ppl'
 
   def install
-    # Cannot build with LLVM (cross compiler crashes)
-    ENV.gcc_4_2
     system "./configure", "--prefix=#{prefix}", "--target=arm-eabi",
                 "--with-gmp=#{Formula.factory('gmp').prefix}",
                 "--with-mpfr=#{Formula.factory('mpfr').prefix}",
                 "--with-ppl=#{Formula.factory('ppl').prefix}",
+                "--with-mpc=#{Formula.factory('libmpc').prefix}",
                 "--with-cloog=#{Formula.factory('cloog-ppl').prefix}",
-                "--disable-werror"
+                "--enable-cloog-backend=ppl", "--disable-werror"
     system "make"
     system "make install"
   end
