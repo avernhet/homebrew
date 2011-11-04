3faec8b465ed1feca3d58d73200e96863069d228
Emmanuel Blot <emmanuel.blot@free.fr>
2011-11-04 17:45:58 +0100

    Upgrade Cloog to 0.16.3

--- Library/Formula/cloog.rb
+++ Library/Formula/cloog.rb
@@ -1,16 +1,16 @@
 require 'formula'
 
 class Cloog <Formula
-  url 'http://www.bastoul.net/cloog/pages/download/cloog-0.16.2.tar.gz'
+  url 'http://www.bastoul.net/cloog/pages/download/cloog-0.16.3.tar.gz'
   homepage 'http://www.bastoul.net/cloog/'
-  sha1 '3bdccfe24e5bd5850cbd28eec70c6aeaa94747ab'
+  sha1 'f6765fa231f38c2f747e2b05e4eaaa470fd5851a'
 
   depends_on 'gmp'
   depends_on 'gnu-libtool' => :build
 
   def install
     system "./configure", "--prefix=#{prefix}",
-                          "--with-gmp=#{Formula.factory('gmp').prefix}",
+                          "--with-gmp=#{Formula.factory('gmp').prefix}"
     # I am SO tired of the autotools mess...
     # Replace the buggy generated libtool with the Homebrew one
     File.unlink "libtool"
