0b82a52ceff6af901497113fb81049ab7d09d391
Emmanuel Blot <emmanuel.blot@free.fr>
2012-02-15 00:46:58 +0100

    Upgrade QEmu to v1.0

--- Library/Formula/qemu.rb
+++ Library/Formula/qemu.rb
@@ -1,14 +1,18 @@
 require 'formula'
 
 class Qemu < Formula
-  url 'http://wiki.qemu.org/download/qemu-0.15.0.tar.gz'
+  url 'http://wiki.qemu.org/download/qemu-1.0.tar.gz'
   homepage 'http://www.qemu.org/'
-  md5 'dbc55b014bcd21b98e347f6a90f7fb6d'
+  md5 'a64b36067a191451323b0d34ebb44954'
 
   depends_on 'jpeg'
   depends_on 'gnutls'
 
   fails_with_llvm "Segmentation faults occur at run-time with LLVM"
+
+  def patches
+    DATA
+  end
 
   def install
     system "./configure", "--prefix=#{prefix}",
@@ -19,3 +23,24 @@ class Qemu < Formula
     system "make install"
   end
 end
+
+__END__
+diff -u a/fpu/softfloat.h b/fpu/softfloat.h
+--- a/fpu/softfloat.h	2011-12-01 21:07:34.000000000 +0100
++++ b/fpu/softfloat.h	2012-02-15 00:33:28.000000000 +0100
+@@ -56,10 +56,14 @@
+ typedef uint8_t flag;
+ typedef uint8_t uint8;
+ typedef int8_t int8;
+-#ifndef _AIX
++#if ! defined (_AIX) && ! defined (__APPLE__)
+ typedef int uint16;
+ typedef int int16;
+ #endif
++#if defined (__APPLE__)
++typedef uint16_t uint16;
++typedef int16_t int16;
++#endif
+ typedef unsigned int uint32;
+ typedef signed int int32;
+ typedef uint64_t uint64;
