ce5085885556d7cd67ffd3cc3814181cf5249688
Emmanuel Blot <emmanuel.blot@free.fr>
2012-02-19 00:29:09 +0100

    Update GDB for ARM-EABI to 7.4

--- Library/Formula/gdb-arm-eabi.rb
+++ Library/Formula/gdb-arm-eabi.rb
@@ -1,15 +1,15 @@
 require 'formula'
 
 class GdbArmEabi <Formula
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.3.1.tar.bz2'
+  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.4.tar.bz2'
   homepage 'http://www.gnu.org/software/gdb/'
-  md5 'b89a5fac359c618dda97b88645ceab47'
+  md5 '95a9a8305fed4d30a30a6dc28ff9d060'
 
   depends_on 'gmp'
   depends_on 'mpfr'
   depends_on 'ppl'
   depends_on 'libmpc'
-  depends_on 'cloog'
+  # depends_on 'cloog' # ./configure does not support CLooG 0.17
 
   def install
     system "./configure", "--prefix=#{prefix}", "--target=arm-eabi",
@@ -17,8 +17,9 @@ class GdbArmEabi <Formula
                 "--with-mpfr=#{Formula.factory('mpfr').prefix}",
                 "--with-ppl=#{Formula.factory('ppl').prefix}",
                 "--with-mpc=#{Formula.factory('libmpc').prefix}",
-                "--with-cloog=#{Formula.factory('cloog').prefix}",
-                "--enable-cloog-backend=isl", "--disable-werror"
+                "--without-cloog", "--disable-werror"
+                #"--with-cloog=#{Formula.factory('cloog').prefix}",
+                #"--enable-cloog-backend=isl", "--disable-werror"
     system "make"
     system "make install"
   end
