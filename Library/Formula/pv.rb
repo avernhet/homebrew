d88ff5087fd3efcf3c306dd0f57fde97a4252ec5
Emmanuel Blot <emmanuel.blot@free.fr>
2012-07-19 14:55:39 +0200

    Upgrade Pipe Viewer to v1.3.4

--- Library/Formula/pv.rb
+++ Library/Formula/pv.rb
@@ -1,15 +1,18 @@
 require 'formula'
 
 class Pv < Formula
-  url 'http://www.ivarch.com/programs/sources/pv-1.2.0.tar.bz2'
+  url 'http://www.ivarch.com/programs/sources/pv-1.3.4.tar.bz2'
   homepage 'http://www.ivarch.com/programs/pv.shtml'
-  md5 '67aedf6dbcd068d5feeaa76156153f4f'
+  md5 '8041eac8a2942ef11ce6c7559cc4fb0a'
 
   fails_with :llvm do
     build 2334
   end
 
   def install
+    # clang cannot be used to repackage object files into a single object file
+    # use regular LD instead
+    ENV['LD'] = 'ld'
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}",
