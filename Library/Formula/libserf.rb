fda4f523e3b5dd9ea0fc7b737f3c086f630c8c8b
Emmanuel Blot <emmanuel.blot@free.fr>
2012-08-10 09:33:00 +0200

    Update libserf to 1.1.0

--- Library/Formula/libserf.rb
+++ Library/Formula/libserf.rb
@@ -1,12 +1,21 @@
 require 'formula'
 
 class Libserf < Formula
-  url 'http://serf.googlecode.com/files/serf-1.0.1.tar.bz2'
+  url 'http://serf.googlecode.com/files/serf-1.1.0.tar.bz2'
   homepage 'http://code.google.com/p/serf/'
-  sha1 '927cac9bbffeb7a60f49ba5ccd1c693d10c94142'
+  sha1 '231af70b7567a753b49df4216743010c193884b7'
+
+  depends_on 'gnu-libtool'
+  depends_on 'openssl'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    # Mac OS X APR is built with non-existing tools
+    ENV['CPP'] = "/usr/bin/cc"
+    ENV['APR_LIBTOOL'] = "#{Formula.factory('gnu-libtool').prefix}/bin/libtool"
+
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--with-openssl=#{Formula.factory('openssl').prefix}",
                           "--prefix=#{prefix}"
     system "make"
     system "make install"
