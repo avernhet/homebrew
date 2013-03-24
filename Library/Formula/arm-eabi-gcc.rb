a429849769aadef375f0f9d535f7c26c156a7398
Emmanuel Blot <eblot.com@gmail.com>
2013-03-24 22:32:34 +0100

    Fix GCC 4.8.0

--- Library/Formula/arm-eabi-gcc.rb
+++ Library/Formula/arm-eabi-gcc.rb
@@ -7,18 +7,17 @@ class ArmEabiNewLib <Formula
 end
 
 class ArmEabiGcc <Formula
-  #url       'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  url 'file:///Users/eblot/Desktop/gcc-4.8.0.tar.bz2'
+  url       'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
   homepage  'http://gcc.gnu.org/'
   sha1      'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
 
   depends_on 'gmp'
-  depends_on 'mpfr'
   depends_on 'libmpc'
-  #depends_on 'ppl'
+  depends_on 'mpfr'
   depends_on 'cloog'
-  depends_on 'libelf'
+  depends_on 'isl'
   depends_on 'arm-eabi-binutils'
+  depends_on 'gcc48'
 
   def patches
     DATA
@@ -36,19 +35,24 @@ class ArmEabiGcc <Formula
         system "ditto", Dir.pwd+'/newlib', coredir+'/newlib'
     }
 
+    gmp = Formula.factory 'gmp'
+    mpfr = Formula.factory 'mpfr'
+    libmpc = Formula.factory 'libmpc'
+    cloog = Formula.factory 'cloog'
+    isl = Formula.factory 'isl'
+    libelf = Formula.factory 'libelf'
+    binutils = Formula.factory 'arm-eabi-binutils'
+    gcc48 = Formula.factory 'gcc48'
+
     # Fix up CFLAGS for cross compilation (default switches cause build issues)
-    ENV['CC'] = "gcc"
-    ENV['CXX'] = "gcc"
+    ENV['CC'] = "#{gcc48.opt_prefix}/bin/gcc-4.8"
+    ENV['CXX'] = "#{gcc48.opt_prefix}/bin/g++-4.8"
     ENV['CFLAGS_FOR_BUILD'] = "-O2"
     ENV['CFLAGS'] = "-O2"
     ENV['CFLAGS_FOR_TARGET'] = "-O2"
     ENV['CXXFLAGS_FOR_BUILD'] = "-O2"
     ENV['CXXFLAGS'] = "-O2"
     ENV['CXXFLAGS_FOR_TARGET'] = "-O2"
-
-    # GCC 4.6.x explictly looks for CLooG 0.16, and we use 0.17
-    # Hack from http://joelinoff.com/blog/?p=108
-    inreplace 'gcc/graphite-clast-to-gimple.c', ' LANGUAGE_C', ' CLOOG_LANGUAGE_C'
 
     build_dir='build'
     mkdir build_dir
@@ -60,16 +64,16 @@ class ArmEabiGcc <Formula
                   "--with-float=soft", "--enable-interwork", "--enable-lto",
                   "--with-multilib-list=interwork",
                   "--with-abi=aapcs", "--enable-languages=c,c++",
-                  "--with-gmp=#{Formula.factory('gmp').prefix}",
-                  "--with-mpfr=#{Formula.factory('mpfr').prefix}",
-                  "--with-mpc=#{Formula.factory('libmpc').prefix}",
-                  "--with-cloog=#{Formula.factory('cloog').prefix}",
+                  "--with-gmp=#{gmp.opt_prefix}",
+                  "--with-mpfr=#{mpfr.opt_prefix}",
+                  "--with-mpc=#{libmpc.opt_prefix}",
+                  "--with-cloog=#{cloog.opt_prefix}",
                   "--enable-cloog-backend=isl",
-                  "--disable-cloog-version-check",
-                  "--with-libelf=#{Formula.factory('libelf').prefix}",
+                  "--with-isl=#{isl.opt_prefix}",
+                  "--with-libelf=#{libelf.opt_prefix}",
                   "--with-gxx-include-dir=#{prefix}/arm-eabi/include",
                   "--disable-debug", "--disable-__cxa_atexit",
-                  "--with-pkgversion=Neotion-SDK2-Celeborn",
+                  "--with-pkgversion=Neotion-SDK2-Gandalf",
                   "--with-bugurl=http://www.neotion.com"
       # Temp. workaround until GCC installation script is fixed
       system "mkdir -p #{prefix}/arm-eabi/lib/fpu/interwork"
@@ -96,26 +100,3 @@ __END__
  # 
  # MULTILIB_OPTIONS    += fno-leading-underscore/fleading-underscore
  # MULTILIB_DIRNAMES   += elf under
---- a/gcc/config/i386/i386.c	2011-06-18 11:07:20.000000000 +0200
-+++ b/gcc/config/i386/i386.c	2011-07-18 16:03:31.000000000 +0200
-@@ -6145,7 +6145,9 @@
-    See the x86-64 PS ABI for details.
- */
- 
--static int
-+int classify_argument (enum machine_mode, const_tree,
-+                       enum x86_64_reg_class [MAX_CLASSES], int);
-+int
- classify_argument (enum machine_mode mode, const_tree type,
- 		   enum x86_64_reg_class classes[MAX_CLASSES], int bit_offset)
- {
-@@ -6526,7 +6528,8 @@
- 
- /* Examine the argument and return set number of register required in each
-    class.  Return 0 iff parameter should be passed in memory.  */
--static int
-+int examine_argument (enum machine_mode, const_tree, int, int *, int *);
-+int
- examine_argument (enum machine_mode mode, const_tree type, int in_return,
- 		  int *int_nregs, int *sse_nregs)
- {
