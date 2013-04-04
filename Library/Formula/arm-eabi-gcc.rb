8102c5908eb4e1ec7a01284ba41c41b7b3a92338
Emmanuel Blot <emmanuel.blot@free.fr>
2013-04-04 17:42:21 +0200

    Update Neotion SDK2 version

--- Library/Formula/arm-eabi-gcc.rb
+++ Library/Formula/arm-eabi-gcc.rb
@@ -73,7 +73,7 @@ class ArmEabiGcc <Formula
                   "--with-libelf=#{libelf.opt_prefix}",
                   "--with-gxx-include-dir=#{prefix}/arm-eabi/include",
                   "--disable-debug", "--disable-__cxa_atexit",
-                  "--with-pkgversion=Neotion-SDK2-Gandalf",
+                  "--with-pkgversion=Neotion-SDK2-Haldir",
                   "--with-bugurl=http://www.neotion.com"
       # Temp. workaround until GCC installation script is fixed
       system "mkdir -p #{prefix}/arm-eabi/lib/fpu/interwork"
