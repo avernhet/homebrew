80e3500e59322c94578967e615154baf3a93bf70
Emmanuel Blot <emmanuel.blot@free.fr>
2013-06-07 15:25:55 +0200

    Upgrade ARM EABI GDB to v7.6

--- Library/Formula/arm-eabi-gdb.rb
+++ Library/Formula/arm-eabi-gdb.rb
@@ -1,44 +1,22 @@
 require 'formula'
 
 class ArmEabiGdb <Formula
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.5.tar.bz2'
+  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.6.tar.bz2'
   homepage 'http://www.gnu.org/software/gdb/'
-  md5 '24a6779a9fe0260667710de1b082ef61'
+  sha1 'b64095579a20e011beeaa5b264fe23a9606ee40f'
 
   depends_on 'gmp'
   depends_on 'mpfr'
   depends_on 'libmpc'
-  #depends_on 'ppl'
-  #depends_on 'cloog'
-
-  def patches
-    DATA
-  end
 
   def install
     system "./configure", "--prefix=#{prefix}", "--target=arm-eabi",
                 "--with-gmp=#{Formula.factory('gmp').prefix}",
                 "--with-mpfr=#{Formula.factory('mpfr').prefix}",
                 "--with-mpc=#{Formula.factory('libmpc').prefix}",
-                #"--with-ppl=#{Formula.factory('ppl').prefix}",
-                #"--with-cloog=#{Formula.factory('cloog').prefix}",
-                #"--enable-cloog-backend=isl",
                 "--without-cloog",
                 "--enable-lto","--disable-werror"
     system "make"
     system "make install"
   end
 end
-
-__END__
---- a/sim/arm/armsupp.c 2012-09-02 12:35:05.000000000 +0200
-+++ b/sim/arm/armsupp.c        2012-09-02 12:34:09.000000000 +0200
-@@ -636,7 +636,7 @@
-   if (! CP_ACCESS_ALLOWED (state, CPNum))
-     {
-       ARMul_UndefInstr (state, instr);
--      return;
-+      return (0);
-     }
- 
-   cpab = (state->MRC[CPNum]) (state, ARMul_FIRST, instr, &result);
